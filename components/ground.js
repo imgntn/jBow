0fe0ca60f744a32062f5229476857542cfc06a6a
James B. Pollack, MFA <james.b.pollack@gmail.com>
2025-06-06 13:44:02 -0700

    Update A-Frame and physics system

--- components/ground.js
+++ components/ground.js
@@ -17,7 +17,8 @@ AFRAME.registerComponent('ground', {
     objectLoader.load(MODEL_URL, function (obj) {
       obj.children.forEach(function (value) {
         value.receiveShadow = true;
-        value.material.shading = THREE.FlatShading;
+        value.material.flatShading = true;
+        value.material.needsUpdate = true;
       });
       object3D.add(obj);
     });
